56f47f77f4a6b32f5a3a1002b2aa3bc6b8c45669
sneha gupta <gsneha20012004@gmail.com>
2024-07-31 15:15:45 +0530

    header component, and added gitignore file

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../assets/investment-calculator-logo.png';
+
+const Header = () => {
+  return (
+    <div className='' id='header'>
+      <img src={logo}></img>
+      <h1>Investment calculator</h1>
+    </div>
+  )
+}
+
+export default Header
